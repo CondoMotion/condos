1e2568f1436ae3f43c950758cd2743bb0ecefd9d
David Van Der Beek <earlynovrock@gmail.com>
2012-11-14 17:30:44 -0600

    slightly modify css for flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+  def current_role
+  	@current_role ||= current_user.email if current_user
+  end
+  helper_method :current_role
+
 end
