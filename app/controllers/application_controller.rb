f62eb2b80fdee74dba3e6619232f956118412c6d
David Van Der Beek <earlynovrock@gmail.com>
2012-11-14 17:59:48 -0600

    add cancan and simple example of how it will work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
 private
   def current_role
-  	@current_role ||= current_user.email if current_user
+  	# UPDATE THIS METHOD TO DETERMINE CURRENT ROLE FOR THE USER
+  	if user_signed_in?
+  		@current_role ||= :admin 
+  	else
+  		@current_role ||= :guest
+  	end
   end
   helper_method :current_role
+
+  def current_ability
+	  @current_ability ||= Ability.new(current_role)
+	end
 
 end
