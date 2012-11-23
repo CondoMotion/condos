e357450f716a86a3c71dc1183d8a6bf494e46d4f
David Van Der Beek <earlynovrock@gmail.com>
2012-11-23 10:53:12 -0600

    add default roles when creating a company

--- app/models/company.rb
+++ app/models/company.rb
@@ -7,6 +7,8 @@ class Company < ActiveRecord::Base
   has_many :users
   has_many :roles
 
+  before_create :add_default_roles
+
   validates_presence_of :name
 
 private
@@ -15,6 +17,20 @@ private
 		@owner.company_id = self.id
 		@owner.manager = 1 
 		@owner.save
+	end
+
+	def add_default_roles
+		@role = self.roles.new(name: "Resident", permission: 1)
+		@role.save
+
+		@role = self.roles.new(name: "Admin", permission: 2)
+		@role.save
+
+		@role = self.roles.new(name: "Trustee", permission: 3)
+		@role.save
+
+		@role = self.roles.new(name: "Manager", permission: 4)
+		@role.save
 	end
 
 end
